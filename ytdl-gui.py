325c1bdbdfb923ce9a9997429ac68b5dadc05096
RationalAsh <ashwinnarayan1994@gmail.com>
2015-06-08 17:27:32 +0800

    It's workinggit add . :D

--- ytdl-gui.py
+++ ytdl-gui.py
@@ -6,6 +6,7 @@ import time
 from datetime import datetime
 import subprocess
 import re
+import pexpect
 #import serial
 #from scipy.signal import
 
@@ -48,12 +49,14 @@ class Handler:
 
     #Function that populates the dropdown menu with available formats
     def getFormats(self):
+        global formatList
         #GLib.idle_add()
         ydl = subprocess.Popen(["youtube-dl", self.link, "-F"], stdout=subprocess.PIPE)
         output = ydl.communicate()[0]
         lines = output.split('\n')
         #fmtBest = re.findall(r'\d+', lines[len(lines)-2])[0]
         fmts = self.extractFormats(output)
+        formatList = fmts
         GLib.idle_add(self.showOutput, output)
         GLib.idle_add(self.updateFormats, fmts)
         #print "This is a thread! :D", self.link
@@ -62,18 +65,18 @@ class Handler:
         global pbar
         pbar.set_fraction(i)
         
-    def vidDownloadThread(link, destination):
-        ydl = subprocess.Popen(["youtube-dl", link, "-F"], stdout=subprocess.PIPE)
-        i = 0
-        while True:
-            inline = ydl.stdout.readline()
-            if not inline:
-                break
-            print inline, destination
-            i += 1
-            fract = (i%100)/100.0
-            Glib.idle_add(self.updateDownloadProgress, fract)
-            time.sleep(0.5)
+    # def vidDownloadThread(link, destination):
+    #     ydl = subprocess.Popen(["youtube-dl", link, "-F"], stdout=subprocess.PIPE)
+    #     i = 0
+    #     while True:
+    #         inline = ydl.stdout.readline()
+    #         if not inline:
+    #             break
+    #         print inline, destination
+    #         i += 1
+    #         fract = (i%100)/100.0
+    #         Glib.idle_add(self.updateDownloadProgress, fract)
+    #         time.sleep(0.5)
         
 
     def onDeleteWindow(self, *args):
@@ -98,13 +101,58 @@ class Handler:
         global textView
         global formatSel
         end_iter = textView.get_buffer().get_end_iter()
-        textView.get_buffer().insert(end_iter, "Downloading "+formatSel.get_active_text()+" file to ")
-        dest = fileChooser.get_filenames()[0]
+        textView.get_buffer().insert(end_iter, "Downloading "+formatSel.get_active_text().split(" ")[0]+" file to ")
+        try:
+            dest = fileChooser.get_filenames()[0]
+        except IndexError:
+            pass
         textView.get_buffer().insert(end_iter, dest+"\n")
-        download_t = threading.Thread(target=self.vidDownloadThread, args=(dest,))
+        download_t = threading.Thread(target=self.vidDownloadThread, args=(dest, formatSel.get_active_text().split(" ")))
         download_t.daemon = True
         download_t.start()
+
+    def vidDownloadThread(self, dest, fmt):
+        global textView
+        global formatSel
+        global formatList
+        global linkEntry
+        global fileName
+
+        link = linkEntry.get_text()
+        video_name = fileName.get_text()
+        video_name += "."+fmt[1]
+        fmt_code = fmt[0]
+
+        print "Saving "+video_name+"to"+dest, fmt, link, 
+
+        #ydl = subprocess.Popen(["youtube-dl", link, "-F"], stdout=subprocess.PIPE)
+        #download = pexpect.spawn('youtube-dl ' + '-f '+ str(fmt_code) + '-o ' + dest + video_name)
+        command_str = 'youtube-dl ' + link + ' -f '+ str(fmt_code) + ' -o ' + dest + "/" + video_name
+        download = pexpect.spawn(command_str)
+        percent = 0.0
+        cont = statusBar.get_context_id("Talk about data")
+        statusBar.push(cont, "Downloading...")
+
+        while True:
+            string = download.read_nonblocking(1000, 10)
+            print string
+            idx = string.find("%")
+            if(idx >= 4):
+                try:
+                    percent = float(string[idx-4:idx])
+                except:
+                    GLib.idle_add(self.updateDownloadProgress, 1.00)
+                    print 'Download thread done'
+                    break
+                if(percent == 100.0):
+                    print 'Download thread done'
+                    GLib.idle_add(self.updateDownloadProgress, 1.00)
+                    break
+                GLib.idle_add(self.updateDownloadProgress, percent/100)
         
+        statusBar.push(cont, "Done! :D")
+            
+            
 
 def app_main():
     #Get the gui from the glade file
@@ -122,6 +170,10 @@ def app_main():
     statusBar = builder.get_object("statusbar1")
     cont = statusBar.get_context_id("Talk about data")
     statusBar.push(cont, "Ready")
+    global linkEntry
+    linkEntry = builder.get_object('entry1')
+    global fileName
+    fileName = builder.get_object('entry2')
 
     global window    
     window = builder.get_object("MainWindow")
